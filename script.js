3c8a3b071602be23bc30d706774be5d6ca20f025
Aditya Wahyudi <adityawahyudi306@gmail.com>
2025-12-03 14:21:31 +0700

    fixes to footer

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const promo = new bootstrap.Modal(document.getElementById('promoModal'));
     promo.show();
 });
+
+// Load Footer
+fetch("footer.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("footer-placeholder").innerHTML = html;
+
+    // Jalankan loader info kalau ada
+    if (typeof loadInformasi === "function") {
+      loadInformasi();
+    }
+  });
+
